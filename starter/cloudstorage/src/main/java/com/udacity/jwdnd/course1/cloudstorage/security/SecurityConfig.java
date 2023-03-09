d9bb1ec41e94d4e991aa75bda8d269a8939e046d
imakida <makida.ian@gmail.com>
2023-03-08 17:35:34 -1000

    feat: add redirect after login

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/security/SecurityConfig.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/security/SecurityConfig.java
@@ -13,5 +13,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
        http.authorizeRequests().antMatchers("/signup", "/css/**", "/js/**").permitAll()
        .anyRequest().authenticated();
        http.formLogin().loginPage("/login").permitAll();
+       http.formLogin().defaultSuccessUrl("/home", true);
     }
 }
