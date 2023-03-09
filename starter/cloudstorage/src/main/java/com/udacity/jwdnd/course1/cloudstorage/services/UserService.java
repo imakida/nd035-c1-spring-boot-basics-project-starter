7503881dfb828767e681039d7ad897f577c40b11
imakida <makida.ian@gmail.com>
2023-03-08 17:31:03 -1000

    feat: create User

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/UserService.java
@@ -0,0 +1,23 @@
+package com.udacity.jwdnd.course1.cloudstorage.services;
+
+import java.security.SecureRandom;
+import java.util.Base64;
+
+import org.springframework.stereotype.Service;
+
+import com.udacity.jwdnd.course1.cloudstorage.models.User;
+import com.udacity.jwdnd.course1.cloudstorage.mappers.UserMapper;
+
+@Service
+public class UserService {
+    private HashService hashService;
+    private UserMapper userMapper;
+    public int createUser(User user){
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[16];
+        random.nextBytes(salt);
+        String encodedSalt = Base64.getEncoder().encodeToString(salt);
+        String hashedPassword = hashService.getHashedValue(user.getPassword(), encodedSalt);
+        return userMapper.insert(new User(null, user.getUsername(), encodedSalt, hashedPassword, user.getFirstName(), user.getLastName()));
+    }
+}
